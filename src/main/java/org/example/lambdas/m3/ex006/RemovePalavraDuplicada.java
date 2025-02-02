341d2cea3e51afa699fc3138b4b24cfa17ae8abf
jvmaiaa <joaovictormaia482@gmail.com>
2025-02-01 23:46:48 -0300

    feat: Variação da remoção de palavras duplicadas utilizando Stream.

--- src/main/java/org/example/lambdas/m3/ex006/RemovePalavraDuplicada.java
+++ src/main/java/org/example/lambdas/m3/ex006/RemovePalavraDuplicada.java
@@ -1,17 +1,20 @@
 package org.example.lambdas.m3.ex006;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 public class RemovePalavraDuplicada {
     public static void main(String[] args) {
         List<String> palavras = Arrays.asList("apple", "banana", "apple", "orange", "banana");
-        Set<String> palavrasUnicas = new HashSet<>();
+        List<String> palavrasUnicas = palavras.stream()
+                .distinct()
+                .collect(Collectors.toList());
+        palavrasUnicas.forEach(System.out::println);
 
-        palavras.stream()
-                .filter(p -> palavrasUnicas.add(p))
-                .forEach(System.out::println);
+        // ou dessa forma:
+        //
+        // Set<String> palavrasUnicas = palavras.stream()
+        //                .collect(Collectors.toSet());
     }
 }
