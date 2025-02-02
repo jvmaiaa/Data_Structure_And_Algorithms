ccadf1b6625bf6fe0ff70f39d475d4ca41f3938d
jvmaiaa <joaovictormaia482@gmail.com>
2025-02-02 19:21:26 -0300

    feat: Exemplo de uso com peek no Stream.

--- src/main/java/org/example/lambdas/m3/ex001/Teste.java
+++ src/main/java/org/example/lambdas/m3/ex001/Teste.java
@@ -1,4 +1,21 @@
 package org.example.lambdas.m3.ex001;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Teste {
+
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+
+        int soma = numeros.stream()
+                .peek(n -> System.out.println("Elemento: " + n))
+                .filter(n -> n % 2 == 1)
+                .peek(n -> System.out.println("Conteúdo depois do filter: " + n))
+                .map(n -> n * 2)
+                .peek(n -> System.out.println("Conteúdo depois do map: " + n))
+                .reduce(0, (total, numero) -> total + numero);
+
+        System.out.println("A soma dos números é: " + soma);
+    }
 }
